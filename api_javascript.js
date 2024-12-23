05404913e7c788cf2947add8922a184383c059c1
Manuel Andres Mendieta <manuelmendieta21@gmail.com>
2024-12-23 13:42:11 -0500

    update fech andpreloaded

--- api_javascript.js
+++ api_javascript.js
@@ -1,16 +1,39 @@
+const urlcarga = 'https://dog.ceo/api/breeds/image/random';
 
-const urlcarga='https://dog.ceo/api/breeds/image/random'
+document.addEventListener('DOMContentLoaded', () => {
+    Generar();
+});
 
+function Generar() {
+    const preloader = document.getElementById('preloader');
+    const imagen2 = document.getElementById('imagendevuelta');
 
-function Generar(){
+    // Mostrar el preloader y ocultar la imagen
+    preloader.style.display = 'block';
+    imagen2.style.display = 'none';
+
+    // Promesa para obtener la información de la URL
     fetch(urlcarga)
-    //callback cambiarlo a un objeto json javascript
-    .then(res => res.json())
-    //otra promesas datos con la informacion 
-    .then(data =>{
-        const imagen = data.message;
-        console.log(imagen)  
-        const imagen2 = document.getElementById('imagendevuelta');
-        imagen2.src = imagen;
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return Promise.reject('Error en la respuesta del servidor');
+            }
         })
+        .then(data => {
+            const imagen = data.message;
+            const status = data.status;
+            console.log(data.status);
+
+            // Ocultar el preloader y mostrar la imagen
+            preloader.style.display = 'none';
+            imagen2.src = imagen;
+            imagen2.style.display = 'block';
+        })
+        .catch(error => {
+            console.error('Error al realizar la solicitud:', error);
+            // Ocultar el preloader en caso de error
+            preloader.style.display = 'none';
+        });
 }
