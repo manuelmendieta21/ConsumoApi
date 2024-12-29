fcf50f16e4ffe855c5299f10d2af218faf2dda96
Manuel Andres Mendieta <manuelmendieta21@gmail.com>
2024-12-29 18:38:17 -0500

    update project new exclude export project

--- api_javascript.js
+++ api_javascript.js
@@ -1,13 +1,19 @@
-export const urlcarga = 'https://dog.ceo/api/breeds/image/random';
-export let favoritos = [];
+const urlcarga = 'https://dog.ceo/api/breeds/image/random';
+let favoritos = [];
 
-export function Generar() {
+document.addEventListener('DOMContentLoaded', () => {
+    Generar();
+});
+
+function Generar() {
     const preloader = document.getElementById('preloader');
     const imagen2 = document.getElementById('imagendevuelta');
 
+    // Mostrar el preloader y ocultar la imagen
     preloader.style.display = 'block';
     imagen2.style.display = 'none';
 
+    // Promesa para obtener la información de la URL
     fetch(urlcarga)
         .then(response => {
             if (response.status === 200) {
@@ -18,25 +24,34 @@ export function Generar() {
         })
         .then(data => {
             const imagen = data.message;
+            
+            // Ocultar el preloader y mostrar la imagen
             preloader.style.display = 'none';
             imagen2.src = imagen;
             imagen2.style.display = 'block';
         })
         .catch(error => {
             console.error('Error al realizar la solicitud:', error);
+            // Ocultar el preloader en caso de error
             preloader.style.display = 'none';
         });
 }
 
-export function Favorito() {
-    const imagen2 = document.getElementById('imagendevuelta');
+function Favorito() {
+    imagen2 = document.getElementById('imagendevuelta');
     const imagen = imagen2.src;
+    console.log(imagen);
+
     const nombre = prompt('Ingrese el nombre del perro favorito');
-    const favorite = {
+    let favorites ={
         name: nombre,
-        image: imagen,
-    };
-    favoritos.push(favorite);
+        image: imagen
+    }
+    localStorage.setItem('favoritos', JSON.stringify(favorites));
+    favoritos.push(favorites);
     localStorage.setItem('favoritos', JSON.stringify(favoritos));
     console.log(favoritos);
+
+
 }
+
